16c26521fb61459a46d97fe03e2a5ae88bb0cc2c
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2024-08-27 12:13:07 +0300

    lisäys toiminto luotu

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import landingview, albumlistview, bandlistview
+from .views import landingview, albumlistview, bandlistview, addband, addalbum
 
 urlpatterns = [
     path('', landingview),
 
     # albumlistview url
     path('albums/', albumlistview),
+    path('add-album/', addalbum),
 
     # bandlistview url
     path('bands/', bandlistview),
+    path('add-band/', addband),
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Band, Album
 
 # Luodaan näkymät
 
 def landingview(request):
     return render(request, 'landingpage.html')
 
+# album näkymä
 def albumlistview(request):
-    return render(request, 'albumlist.html')
+    albumlist = Album.objects.all()
+    bandlist = Band.objects.all()
+    context = {'albums': albumlist, 'band': bandlist}
+    return render(request, "albumlist.html", context)
 
+# albumin lisäys näkymä
+def addalbum(request):
+    a = request.POST['name']
+    b = request.POST['band']
+    c = request.POST['year']
+    Album(name=a, band=Band.objects.get(id=b), year=c).save()
+    return redirect(request.META['HTTP_REFERER'])
+
+# bändi näkymä
 def bandlistview(request):
-    return render(request, 'bandlist.html')
+    bandlist = Band.objects.all()
+    context = {'bands': bandlist}
+    return render(request, "bandlist.html", context)
+
+# bändin lisäys näkymä
+def addband(request):
+    a = request.POST['name']
+    b = request.POST['country']
+    c = request.POST['genre']
+    d = request.POST['year']
+    Band(name=a, country=b, genre=c, year=d).save()
+    return redirect(request.META['HTTP_REFERER'])
