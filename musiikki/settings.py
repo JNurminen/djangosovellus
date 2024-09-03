14e02bacb95f8576b71a2ef9584621620d3918b6
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2024-09-03 11:51:25 +0300

    postgresql

--- musiikki/settings.py
+++ musiikki/settings.py
@@ -73,11 +73,23 @@ WSGI_APPLICATION = 'musiikki.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+# Muutettu tietokanta postgresql:ksi
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'musiikki_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Tammikuu.24',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
